55e5077be00a97aaee92f1a53ddf764f191c386f
Yusra Yasir <https://github.com/YusraYasir/simple_calculator.git>
2024-03-30 13:50:45 -0700

    Todo list Project

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+import inquirer  from "inquirer";
+import  chalk  from  "chalk";
+
+let todoList = [];
+let condition = true;
+
+console.log(chalk.magenta.bold("\n\tWelcome to code With Yusra - Todo-List Application\n"));
+
+while(condition){
+
+    let addTask = await inquirer.prompt([
+
+        {
+            name: "task",
+            type: "input",
+            message:"What you want to add in your Todo-list :"
+        }
+    ]);
+
+    todoList.push(addTask.task);
+    console.log(chalk.green(`\n\t${addTask.task} Task added in Todo-list sucessfully\n`));
+
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more task ?",
+            default: "false"
+        }
+    ]) ;
+     condition = addMoreTask.addmore
+}
+
+console.log("Your updated Todo-List:", todoList);
+
+
